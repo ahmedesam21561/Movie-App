2301868756942fa65a10f23fdb5bdd95c7461075
ahmedesam21561 <ahmedesam21561@gmail.com>
2023-12-26 14:42:53 +0200

    six commit

--- JS/main.js
+++ JS/main.js
@@ -1,99 +1,92 @@
-var siteName = document.getElementById("sn");
-var siteURL = document.getElementById("su");
-var allWebsites = [];
-var transferbtn = document.getElementById("add_btn");
-var index = 0;
+var Username = document.getElementById("name");
+var Email = document.getElementById("email");
+var Password = document.getElementById("password");
 
-function addNewProduct() {
+var invalidData = document.getElementById("invalid_data");
+var success_login = document.getElementById("success");
+var signUpArray = [];
 
-    var webSiteData = {
-        site_name: siteName.value,
-        site_url: siteURL.value
-    }
-    if (validateURL(webSiteData.site_url) == true) {
+var getArrayUsers = [];
+var Login = document.getElementById("login");
 
-        if (transferbtn.innerHTML == "Submit") {
-            allWebsites.push(webSiteData);
+var signUp = document.getElementById("signup");
+var userNameH1 = document.getElementById('username');
+
+
+function login() {
+
+    if (Login.innerHTML == "Sign Up") {
+        var user = {
+            name: Username.value,
+            mail: Email.value,
+            pass: Password.value
+        }
+        signUpArray.push(user);
+        invalidData.classList.remove('d-none');
+        invalidData.classList.add('text-success')
+        invalidData.innerHTML = "Sign Up Successed";
+        ClearElement();
+        console.log(signUpArray);
+
+        Signup();
+        localStorage.setItem(`users`, JSON.stringify(signUpArray));
+
+        // for (let i = 0; i < signUpArray.length; i++) {
+
+        //     localStorage.setItem(`usersMail`, signUpArray[i].name);
+        //     localStorage.setItem(`usersMail`, signUpArray[i].mail);
+        //     localStorage.setItem(`usersPass`, signUpArray[i].pass);
+
+        // }
+
+
+    } else if (Login.innerHTML == "Log in") {
+        getArrayUsers = JSON.parse(localStorage.getItem('users'));
+        if (getArrayUsers.length == 0) {
+            invalidData.innerHTML = "invalid username or password!";
+            invalidData.classList.remove('d-none');
         } else {
-            for (let i = 0; i < allWebsites.length; i++) {
-                if (index == i) {
-                    allWebsites.splice(i, 1, webSiteData)
+            for (let i = 0; i < getArrayUsers.length; i++) {
+                if (Email.value == getArrayUsers[i].mail && Password.value == getArrayUsers[i].pass) {
+                    invalidData.innerHTML = "Log in Successed";
+                    // userNameH1.innerHTML = "Welcome" + getArrayUsers[i].name;
+                    Login.setAttribute('href', 'home.html');
+                } else {
+                    invalidData.innerHTML = "invalid username or password!";
+                    invalidData.classList.remove('d-none');
+
                 }
             }
-            transferbtn.innerHTML = "Submit";
+            if (invalidData.innerHTML != "Log in Successed") {
+                invalidData.classList.remove('d-none');
+            }
         }
-        console.log(allWebsites);
-        clearelement();
-        displayProducts();
+    }
+
+}
+
+function Signup() {
+    if (signUp.innerHTML == "Sign Up") {
+        Username.classList.remove('d-none');
+        Login.innerHTML = "Sign Up";
+        signUp.innerHTML = "Log in";
     } else {
-        alert("please enter valid URL such(should start with 'www.' and should end with '.com')");
+        Username.classList.add('d-none');
+        Login.innerHTML = "Log in";
+        signUp.innerHTML = "Sign Up";
     }
 
-}
-
-function clearelement() {
-    siteName.value = "";
-    siteURL.value = "";
-}
-
-function displayProducts() {
-    var test = "";
-    var index = 0;
-    // <td>${allWebsites[i].Name}</td>
-    //<td><a href="http://${allWebsites[i].URL}" class="btn btn-success" target="_blank">Visit</a></td>
-    for (var i = 0; i < allWebsites.length; i++) {
-        test += `<tr>
-        <td>${index=i+1}</td>
-        <td>${allWebsites[i].site_name}</td>
-        <td><a href="http://${allWebsites[i].site_url}" class="btn btn-success" target="_blank">Visit</a></td>
-        <td><button type="button" onclick="deleteProduct(${i})" class="btn btn-danger">X</button></td>
-        <td><button type="button" onclick="updateProduct(${i})" class="btn btn-primary">Update</button></td>
-    </tr>`;
-    }
-    document.getElementById("tbody").innerHTML = test;
-}
-
-
-function deleteProduct(id) {
-
-    allWebsites.splice(id, 1);
-    clearelement();
-    displayProducts();
 
 }
 
-function updateProduct(i) {
-
-    transferbtn.innerHTML = "Update";
-    // console.log(transferbtn);
-    siteName.value = allWebsites[i].site_name;
-    siteURL.value = allWebsites[i].site_url;
-    index = i;
+function ClearElement() {
+    Username.value = "";
+    Email.value = "";
+    Password.value = "";
 
 }
 
-function searchElement(term) {
-    var test = "";
-    var index = 0;
-    for (let i = 0; i < allWebsites.length; i++) {
-        if (allWebsites[i].site_name.toLowerCase().includes(term.trim().toLowerCase()) == true) {
-            test += `
-            <td>${index=i+1}</td>
-            <td>${allWebsites[i].site_name}</td>
-            <td><a href="http://${allWebsites[i].site_url}" class="btn btn-success" target="_blank">Visit</a></td>
-            <td><button type="button" onclick="deleteProduct(${i})" class="btn btn-danger">X</button></td>
-            <td><button type="button" onclick="updateProduct(${i})" class="btn btn-primary">Update</button></td>
-        </tr>       
-            `
-        }
-    }
 
-    document.getElementById("tbody").innerHTML = test;
+signUp.addEventListener('click', Signup)
 
-}
-
-function validateURL(urlsite) {
-    var regx = /^w{3}\.[a-z]{2,}\.com$/gi;
-    // var regx2 = /^[(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)%/gi;
-    return regx.test(urlsite);
-}
+Login.addEventListener('click', login);
